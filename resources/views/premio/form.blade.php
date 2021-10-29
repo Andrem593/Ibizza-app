4f880e2530627c36a07948b8b7e9c9ee92ea77a6
Christian Ramos <crismas06@outlook.com>
2021-10-29 17:07:48 -0500

    premios

--- resources/views/premio/form.blade.php
+++ resources/views/premio/form.blade.php
@@ -22,7 +22,8 @@
             <div id="nueva_condicion">
                 <div class="row">
                     <div class="col">
-                        <select id="nombre_tabla" class="selectpicker show-tick" data-live-search="true" data-width="100%">
+                        <select id="nombre_tabla" class="selectpicker show-tick" data-live-search="true"
+                            data-width="100%">
                             <option value="0">Seleccionar tabla</option>
                             <option value="marcas" data-tokens="">Marca</option>
                             <option value="empresarias" data-tokens="">Empresaria</option>
@@ -70,9 +71,21 @@
                             <button id="btn_regla" class="btn btn-ibizza"><i class="fas fa-plus"></i></button>
                         </div>
                     </div>
-                </div>            
+                </div>
             </div>
-            <button id="btn_condicion" class="btn btn-ibizza btn-block btn-sm d-none"><i class="fas fa-plus"></i></button>
+            <button id="btn_condicion" class="btn btn-ibizza btn-block btn-sm d-none"><i class="fas fa-plus"></i>
+                Agregar condición</button>
+            <div class="table-responsive p-3">
+                <table id="example" class="display table table-striped table-sm table-hover fw-bold">
+                    <thead class="bg-ibizza text-center">
+                        <th>item</th>
+                        <th>detalle</th>
+                    </thead>
+                    <tbody class="text-center">
+
+                    </tbody>
+                </table>
+            </div>
         </div>
         {{-- <div class="form-group">
             {{ Form::label('condición') }}
@@ -88,22 +101,21 @@
     <script>
         $(document).ready(function() {
 
-            function inicializar_select(){
+            function inicializar_select() {
                 $(".editable").select2({
-                tags: true
+                    tags: true
                 });
-                $(".no-editable").select2({
-                });
+                $(".no-editable").select2({});
             }
 
-            $('#nombre_tabla').on('change', function(e){
-                
+            $('#nombre_tabla').on('change', function(e) {
+
                 let valor = $('#nombre_tabla').val();
                 console.log(valor);
-                if(valor != '0'){
+                if (valor != '0') {
                     $('#nueva_regla').removeClass('d-none');
                     $('#btn_condicion').removeClass('d-none');
-                }else{
+                } else {
                     $('#nueva_regla').addClass('d-none');
                     $('#btn_condicion').addClass('d-none');
                 }
@@ -111,20 +123,29 @@
 
             inicializar_select();
 
-            $('#btn_regla').on('click', function(e){
+            $('#btn_regla').on('click', function(e) {
                 e.preventDefault();
-                $('#nueva_regla').append('<div class="regla row py-2"> <div class="col col-sm-4"> <select name="table_name" class="no-editable" data-width="100%"> <option value="">Seleccionar campo</option> <option value="marcas">Descripción</option> <option value="empresarias">Empresaria</option> <option value="pedidos">Venta</option> </select> </div> <div class="col col-sm-2"> <select name="operador" class="no-editable" data-width="100%"> <option value="">Operador</option> <option value="marcas">igual</option> <option value="empresarias">mayor que</option> <option value="pedidos">menor que</option> <option value="empresarias">mayor igual que</option> <option value="pedidos">menor igual que</option> <option value="pedidos">contiene</option> <option value="pedidos">no contiene</option> </select> </div> <div class="col col-sm-4"> <select name="valor" class="editable" data-width="100%"> <option value="">Ingrese un valor</option> <option value="marcas">Nueva</option> <option value="empresarias">Compras consecutivas</option> <option value="pedidos">Venta</option> </select> </div> <div class="col col-sm-1"> <select name="condicion" class="no-editable" data-width="100%"> <option value="and">Y</option> <option value="or">O</option> </select> </div> <div class="col col-sm-1"> <button class="btn btn-danger removeRegla"><i class="fas fa-minus"></i></button> </div> </div>');
+                $('#nueva_regla').append(
+                    '<div class="regla row py-2"> <div class="col col-sm-4"> <select name="table_name" class="no-editable" data-width="100%"> <option value="">Seleccionar campo</option> <option value="marcas">Descripción</option> <option value="empresarias">Empresaria</option> <option value="pedidos">Venta</option> </select> </div> <div class="col col-sm-2"> <select name="operador" class="no-editable" data-width="100%"> <option value="">Operador</option> <option value="marcas">igual</option> <option value="empresarias">mayor que</option> <option value="pedidos">menor que</option> <option value="empresarias">mayor igual que</option> <option value="pedidos">menor igual que</option> <option value="pedidos">contiene</option> <option value="pedidos">no contiene</option> </select> </div> <div class="col col-sm-4"> <select name="valor" class="editable" data-width="100%"> <option value="">Ingrese un valor</option> <option value="marcas">Nueva</option> <option value="empresarias">Compras consecutivas</option> <option value="pedidos">Venta</option> </select> </div> <div class="col col-sm-1"> <select name="condicion" class="no-editable" data-width="100%"> <option value="and">Y</option> <option value="or">O</option> </select> </div> <div class="col col-sm-1"> <button class="btn btn-danger removeRegla"><i class="fas fa-minus"></i></button> </div> </div>'
+                    );
                 inicializar_select();
             });
 
-            $('#btn_condicion').on('click', function(e){
-                e.preventDefault();
-                $('#nueva_condicion').append('<div class="row"> <div class="col"> <select id="nombre_tabla" class="selectpicker show-tick" data-live-search="true" data-width="100%"> <option value="0">Seleccionar tabla</option> <option value="marcas" data-tokens="">Marca</option> <option value="empresarias" data-tokens="">Empresaria</option> <option value="pedidos" data-tokens="">Venta</option> </select> </div> </div> <div id="nueva_regla" class="d-none"> <div class="row py-2"> <div class="col col-sm-4"> <select name="table_name" class="no-editable" data-width="100%"> <option value="">Seleccionar campo</option> <option value="marcas">Descripción</option> <option value="empresarias">Empresaria</option> <option value="pedidos">Venta</option> </select> </div> <div class="col col-sm-2"> <select name="operador" class="no-editable" data-width="100%"> <option value="">Operador</option> <option value="marcas">igual</option> <option value="empresarias">mayor que</option> <option value="pedidos">menor que</option> <option value="empresarias">mayor igual que</option> <option value="pedidos">menor igual que</option> <option value="pedidos">contiene</option> <option value="pedidos">no contiene</option> </select> </div> <div class="col col-sm-4"> <select name="valor" class="editable" data-width="100%"> <option value="">Ingrese un valor</option> <option value="marcas">Nueva</option> <option value="empresarias">Compras consecutivas</option> <option value="pedidos">Venta</option> </select> </div> <div class="col col-sm-1"> <select name="condicion" class="no-editable" data-width="100%"> <option value="and">Y</option> <option value="or">O</option> </select> </div> <div class="col col-sm-1"> <button id="btn_regla" class="btn btn-ibizza"><i class="fas fa-plus"></i></button> </div> </div> </div>');
-                inicializar_select();
-            });
-
-            $(document).on('click','button.removeRegla',function(){
+            $(document).on('click', 'button.removeRegla', function() {
                 $(this).closest('div.regla').remove();
+            });
+
+            $('#example').DataTable({
+                data: [],
+                columns: [{
+                        'data': 'item'
+                    },
+                    {
+                        'data': 'detalle'
+                    },
+                ],
+                responsive: false,
+                autoWidth: false,
             });
         });
     </script>
