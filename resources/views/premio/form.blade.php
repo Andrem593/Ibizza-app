b84ae7969ac6841b643d6b34ecc892f0bd44cfab
Christian Ramos <crismas06@outlook.com>
2021-11-04 16:26:13 -0500

    premios

--- resources/views/premio/form.blade.php
+++ resources/views/premio/form.blade.php
@@ -32,9 +32,9 @@
                     </div>
                 </div>
                 <div id="nueva_regla" class="d-none">
-                    <div class="row py-2">
+                    <div class="row py-2 clone-div">
                         <div class="col col-sm-4">
-                            <select name="table_name" class="no-editable" data-width="100%">
+                            <select name="nombreTabla" class="nombreTabla no-editable" data-width="100%">
                                 <option value="">Seleccionar campo</option>
                                 <option value="marcas">Descripción</option>
                                 <option value="empresarias">Empresaria</option>
@@ -42,19 +42,19 @@
                             </select>
                         </div>
                         <div class="col col-sm-2">
-                            <select name="operador" class="no-editable" data-width="100%">
+                            <select name="operador" class="operador no-editable" data-width="100%">
                                 <option value="">Operador</option>
-                                <option value="marcas">igual</option>
-                                <option value="empresarias">mayor que</option>
-                                <option value="pedidos">menor que</option>
-                                <option value="empresarias">mayor igual que</option>
-                                <option value="pedidos">menor igual que</option>
-                                <option value="pedidos">contiene</option>
-                                <option value="pedidos">no contiene</option>
+                                <option value="=">igual</option>
+                                <option value=">">mayor que</option>
+                                <option value="<">menor que</option>
+                                <option value=">=">mayor igual que</option>
+                                <option value="<=">menor igual que</option>
+                                <option value="contiene">contiene</option>
+                                <option value="no contiene">no contiene</option>
                             </select>
                         </div>
                         <div class="col col-sm-4">
-                            <select name="valor" class="editable" data-width="100%">
+                            <select name="valor" class="valor editable" data-width="100%">
                                 <option value="">Ingrese un valor</option>
                                 <option value="marcas">Nueva</option>
                                 <option value="empresarias">Compras consecutivas</option>
@@ -62,7 +62,7 @@
                             </select>
                         </div>
                         <div class="col col-sm-1">
-                            <select name="condicion" class="no-editable" data-width="100%">
+                            <select name="condicion" class="condicion no-editable" data-width="100%">
                                 <option value="and">Y</option>
                                 <option value="or">O</option>
                             </select>
@@ -99,7 +99,56 @@
 </div>
 @push('js')
     <script>
+        let espanol = {
+            "sProcessing": '<div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status"><span class="sr-only">Loading...</span></div>',
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            },
+            "buttons": {
+                "copy": "Copiar",
+                "colvis": "Visibilidad"
+            }
+        }
         $(document).ready(function() {
+            let arrayFinal = [];
+
+            let clone = $("#nueva_condicion").clone();
+            console.log(clone);
+
+            let dataTableCondiciones = $('#example').DataTable({
+                "data": [],
+                "columns": [{
+                        'data': 'nombre_tabla'
+                    },
+                    {
+                        'data': 'descripcion'
+                    },
+                ],
+                "paging": false,
+                "ordering": false,
+                "info": false,
+                "responsive": false,
+                "autoWidth": false,
+                "language": espanol
+            });
 
             function inicializar_select() {
                 $(".editable").select2({
@@ -123,30 +172,107 @@
 
             inicializar_select();
 
+            $('#btn_condicion').on('click', function(e) {
+                e.preventDefault();
+
+                let nombreTablaArray = $.map($(".nombreTabla"), function(element) {
+                    return {
+                        name: element.options[element.selectedIndex].text,
+                        value: element.value
+                    };
+                });
+                let operadorArray = $.map($(".operador"), function(element) {
+                    return {
+                        name: element.options[element.selectedIndex].text,
+                        value: element.value
+                    };
+                });
+                let valorArray = $.map($(".valor"), function(element) {
+                    return {
+                        name: element.options[element.selectedIndex].text,
+                        value: element.value
+                    };
+                });
+                console.log(operadorArray);
+                let condicionArray = $.map($(".condicion"), function(element) {
+                    return {
+                        name: element.options[element.selectedIndex].text,
+                        value: element.value
+                    };
+                });
+
+                let flag = 0;
+                $.each(nombreTablaArray, function(key, value) {
+                    if (value.value == '' || operadorArray[key].value == '' || valorArray[key]
+                        .value == '') {
+                        flag = 1;
+                    }
+                });
+
+                if (!flag) {
+
+                    let condiciones = '';
+                    let nombres = '';
+                    $.each(nombreTablaArray, function(key, value) {
+                        console.log(key);
+                        condiciones += ' ' + value.value + ' ' + operadorArray[key].value + ' ' +
+                            valorArray[key].value + ' ' + condicionArray[key].value;
+                        nombres += ' ' + value.name + ' ' + operadorArray[key].name + ' ' +
+                            valorArray[key].name + ' ' + condicionArray[key].name;
+                    });
+                    let condPart = condiciones.split(' ');
+                    condPart.pop();
+                    let condClean = condPart.toString().replace(/,/g, ' ');
+
+                    let namePart = nombres.split(' ');
+                    namePart.pop();
+                    let nameClean = namePart.toString().replace(/,/g, ' ');
+
+                    let tabla = $('#nombre_tabla').val();
+
+                    arrayFinal.push({
+                        'nombre_tabla' : tabla,
+                        'condicion' : condClean,
+                        'descripcion' : nameClean
+                    });
+                    console.log(arrayFinal);
+
+                    // let result = JSON.parse(arrayFinal);
+                    // console.log(result);
+                    dataTableCondiciones.clear().draw();
+                    dataTableCondiciones.rows.add(arrayFinal).draw();
+
+                    $('#nueva_condicion').removeClass('d-none');
+                    $('#nueva_regla').removeClass('d-none');
+                    $('#btn_condicion').removeClass('d-none');
+
+                    $('#nueva_condicion').empty();
+                    
+                    let someText = $.parseHTML(clone[0].innerHTML);
+                    console.log(someText);
+                    $('#nueva_condicion').append(someText);
+
+                } else {
+                    alert('Hay condiciones incompletas, por favor revisar');
+                }
+            });
+
             $('#btn_regla').on('click', function(e) {
                 e.preventDefault();
+
                 $('#nueva_regla').append(
-                    '<div class="regla row py-2"> <div class="col col-sm-4"> <select name="table_name" class="no-editable" data-width="100%"> <option value="">Seleccionar campo</option> <option value="marcas">Descripción</option> <option value="empresarias">Empresaria</option> <option value="pedidos">Venta</option> </select> </div> <div class="col col-sm-2"> <select name="operador" class="no-editable" data-width="100%"> <option value="">Operador</option> <option value="marcas">igual</option> <option value="empresarias">mayor que</option> <option value="pedidos">menor que</option> <option value="empresarias">mayor igual que</option> <option value="pedidos">menor igual que</option> <option value="pedidos">contiene</option> <option value="pedidos">no contiene</option> </select> </div> <div class="col col-sm-4"> <select name="valor" class="editable" data-width="100%"> <option value="">Ingrese un valor</option> <option value="marcas">Nueva</option> <option value="empresarias">Compras consecutivas</option> <option value="pedidos">Venta</option> </select> </div> <div class="col col-sm-1"> <select name="condicion" class="no-editable" data-width="100%"> <option value="and">Y</option> <option value="or">O</option> </select> </div> <div class="col col-sm-1"> <button class="btn btn-danger removeRegla"><i class="fas fa-minus"></i></button> </div> </div>'
-                    );
+                    '<div class="regla row py-2"> <div class="col col-sm-4"> <select name="nombreTabla" class="nombreTabla no-editable" data-width="100%"> <option value="">Seleccionar campo</option> <option value="marcas">Descripción</option> <option value="empresarias">Empresaria</option> <option value="pedidos">Venta</option> </select> </div> <div class="col col-sm-2"> <select name="operador" class="operador no-editable" data-width="100%"> <option value="">Operador</option> <option value="=">igual</option> <option value=">">mayor que</option> <option value="<">menor que</option> <option value=">=">mayor igual que</option> <option value="<=">menor igual que</option> <option value="contiene">contiene</option> <option value="no contiene">no contiene</option> </select> </div> <div class="col col-sm-4"> <select name="valor" class="valor editable" data-width="100%"> <option value="">Ingrese un valor</option> <option value="marcas">Nueva</option> <option value="empresarias">Compras consecutivas</option> <option value="pedidos">Venta</option> </select> </div> <div class="col col-sm-1"> <select name="condicion" class="condicion no-editable" data-width="100%"> <option value="and">Y</option> <option value="or">O</option> </select> </div> <div class="col col-sm-1"> <button class="btn btn-danger removeRegla"><i class="fas fa-minus"></i></button> </div> </div>'
+                );
                 inicializar_select();
             });
 
-            $(document).on('click', 'button.removeRegla', function() {
+            $(document).on('click', 'button.removeRegla', function(e) {
+                e.preventDefault();
                 $(this).closest('div.regla').remove();
             });
 
-            $('#example').DataTable({
-                data: [],
-                columns: [{
-                        'data': 'item'
-                    },
-                    {
-                        'data': 'detalle'
-                    },
-                ],
-                responsive: false,
-                autoWidth: false,
-            });
+
+
         });
     </script>
 
