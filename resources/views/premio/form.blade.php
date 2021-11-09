a978cc513a5270a8c87440a3616f322b3780c60d
Christian Ramos <crismas06@outlook.com>
2021-11-09 09:48:45 -0500

    premios

--- resources/views/premio/form.blade.php
+++ resources/views/premio/form.blade.php
@@ -13,7 +13,8 @@
                     <select name="catalogo_id" class="selectpicker show-tick" data-live-search="true" data-width="100%">
                         <option value="">Seleccionar un catálogo</option>
                         @foreach ($catalogo as $item)
-                            <option value="{{ $item->id }}" data-tokens="{{ $item->nombre }}">{{ $item->nombre }}
+                            <option value="{{ $item->id }}" data-tokens="{{ $item->nombre }}">
+                                {{ $item->nombre }}
                             </option>
                         @endforeach
                     </select>
@@ -24,25 +25,49 @@
                 <div class="form-group">
 
                     {{ Form::label('condición') }}
-        
+
                     <livewire:condicion />
-        
+
                 </div>
             </div>
 
         </div>
-        
-        <div class="table-responsive p-3">
-            <table id="example" class="display table table-striped table-sm table-hover fw-bold">
-                <thead class="bg-ibizza text-center">
-                    <th>Descripción</th>
-                    <th>Condición</th>
-                    <th></th>
-                </thead>
-                <tbody class="text-center">
 
-                </tbody>
-            </table>
+        <div class="row">
+            <div class="col-4">
+                <div class="table-responsive p-3">
+                    <table id="datatable" class="table table-striped table-hover">
+                        <thead class="bg-ibizza text-center">
+                            <tr>
+                                <th>Seleccionar<br>
+                                    <div class="form-check"><input id="all" class="form-check-input"
+                                            type="checkbox">
+                                    </div>
+                                </th>
+                                <th>Foto</th>
+                                <th>Producto</th>
+                            </tr>
+                        </thead>
+                        <tbody class="text-center">
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col">
+                <div class="table-responsive p-3">
+                    <table id="example" class="display table table-striped table-sm table-hover fw-bold">
+                        <thead class="bg-ibizza text-center">
+                            <th>Descripción</th>
+                            <th>Condición</th>
+                            <th></th>
+                        </thead>
+                        <tbody class="text-center">
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
         {{-- <div class="form-group">
             {{ Form::label('condición') }}
@@ -85,7 +110,111 @@
             }
         }
         $(document).ready(function() {
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+            });
+
             let arrayFinal = [];
+
+            function inicializar_select() {
+                $(".editable").select2({
+                    tags: true
+                });
+                $(".no-editable").select2({
+                    minimumResultsForSearch: Infinity
+                });
+            }
+
+            inicializar_select();
+
+            let dataTable = $('#datatable').DataTable({
+
+                destroy: true,
+                "processing": true,
+                // "ajax": {
+                //     "url": ruta,
+                //     "method": "POST",
+                //     "data": data,
+                //     "dataSrc": function(json) {
+                //         if (json == 'no data') {
+                //             return [];
+                //         } else {
+                //             return json;
+                //         }
+                //     },
+                // },
+                "data": [],
+                "columns": [{
+                        "data": "id",
+                        "render": function(data, type, row) {
+                            let estado =
+                                '<div class="form-check"><input class="form-check-input chk_seleccionar" type="checkbox"></div>';
+                            if (row['en_catalogo'] != null) {
+                                estado =
+                                    '<div class="form-check"><input class="form-check-input chk_seleccionar" type="checkbox" checked></div>'
+                            }
+                            return estado;
+                        }
+                    },
+                    {
+                        "data": "imagen_path",
+                        "render": function(data, type, row) {
+                            let image =
+                                'https://www.blackwallst.directory/images/NoImageAvailable.png';
+                            if (data != '' && data != null) {
+                                image = '/storage/images/productos/' + data
+                            }
+                            return '<center><img  src="' + image +
+                                '"class="rounded" width="80" height="60" /> </center>';
+                        }
+                    },
+                    {
+                        "data": "nombre_producto"
+                    },
+                ],
+                "lengthMenu": [
+                    [-1, 10, 25, 50],
+                    ["Todo", 10, 25, 50]
+                ],
+                "columnDefs": [{
+                        "targets": [0, 1],
+                        "orderable": false,
+                        "searchable": false
+                    },
+                    //{ "width": "1%", "targets": 0 }
+                ],
+                "order": [
+                    [2, 'asc']
+                ],
+                "language": espanol,
+                //para usar los botones
+                responsive: false,
+                autoWidth: false,
+                initComplete: function(row, data, start, end, display) {
+                    //$(document).on('click', '.btn_asignar', myBtnAsignar);
+                },
+                dom: "Bfrtilp",
+                buttons: {
+                    buttons: [{
+                        text: "Seleccionar Premios",
+                        action: function(e, dt, node, config) {
+                            //trigger the bootstrap modal
+                        }
+                    }],
+                    dom: {
+                        button: {
+                            tag: "button",
+                            className: "btn btn-ibizza btn_asignar"
+                        },
+                        buttonLiner: {
+                            tag: null
+                        }
+                    }
+                }
+            });
 
             let dataTableCondiciones = $('#example').DataTable({
                 "data": [],
@@ -111,14 +240,7 @@
                 "language": espanol
             });
 
-            function inicializar_select() {
-                $(".editable").select2({
-                    tags: true
-                });
-                $(".no-editable").select2({
-                    minimumResultsForSearch: Infinity
-                });
-            }
+            
 
             $('#nombre_tabla').on('change', function(e) {
 
@@ -126,21 +248,25 @@
                 console.log(valor);
                 if (valor != '0') {
 
-                    if(valor == 'empresarias'){
-                        $('.campo').html('<option value="" selected>Seleccionar campo</option><option value="tipo_cliente">Tipo Cliente</option>');
-                    }else if(valor == 'pedidos'){
-                        $('.campo').html('<option value="" selected>Seleccionar campo</option><option value="total_factura">Total factura</option>');
+                    if (valor == 'empresarias') {
+                        $('.campo').html(
+                            '<option value="" selected>Seleccionar campo</option><option value="tipo_cliente">Tipo Cliente</option>'
+                            );
+                    } else if (valor == 'pedidos') {
+                        $('.campo').html(
+                            '<option value="" selected>Seleccionar campo</option><option value="total_factura">Total factura</option>'
+                            );
                     }
 
                     $('#nueva_regla').removeClass('d-none');
-                    $('#btn_condicion').removeClass('d-none');                    
+                    $('#btn_condicion').removeClass('d-none');
                 } else {
                     $('#nueva_regla').addClass('d-none');
                     $('#btn_condicion').addClass('d-none');
                 }
             });
 
-            inicializar_select();
+            
 
             $('#btn_condicion').on('click', function(e) {
                 e.preventDefault();
@@ -216,8 +342,8 @@
 
                     Livewire.hook('message.processed', (message, component) => {
                         inicializar_select();
-                    })                    
-                    
+                    })
+
 
                 } else {
                     alert('Hay condiciones incompletas, por favor revisar');
@@ -228,11 +354,12 @@
                 e.preventDefault();
                 $("#nueva_regla").find('select').select2('destroy');
                 let liItem = $('.clone-div:first').clone();
-                liItem.find('.remover_regla').html('<button class="btn btn-danger removeRegla"><i class="fas fa-minus"></i></button>');
+                liItem.find('.remover_regla').html(
+                    '<button class="btn btn-danger removeRegla"><i class="fas fa-minus"></i></button>');
                 //Append the clone to the list item. But this only works once!
                 $('#nueva_regla').append(liItem);
-                
-               inicializar_select();
+
+                inicializar_select();
             });
 
             $('#example').on('click', '.remove', function(e) {
@@ -244,7 +371,7 @@
                     .remove()
                     .draw();
                 console.log(arrayFinal);
-                
+
             });
 
             $(document).on('click', 'button.removeRegla', function(e) {
