146551a98061f29e8d30f46f622761297df12de4
Christian Ramos <crismas06@outlook.com>
2021-11-25 16:04:12 -0500

    quickview

--- resources/views/livewire/modal-quick-view.blade.php
+++ resources/views/livewire/modal-quick-view.blade.php
@@ -32,7 +32,7 @@
                     </div>
                     <div class="col-md-7 col-sm-12 col-xs-12">
                         <div class="quickview-pro-content">
-                            <h5 class="ec-quick-title clasificacion">{{$productos_color[0]->clasificacion}}</h5>
+                            <h5 class="ec-quick-title clasificacion">{{ isset($productos_color) ? $productos_color[0]->clasificacion : '' }}</h5>
                             <input type="hidden" wire:model="productos_color">
                             <div class="ec-quickview-rating">
                                 <i class="ecicon eci-star fill"></i>
@@ -42,41 +42,41 @@
                                 <i class="ecicon eci-star"></i>
                             </div>
 
-                            <div class="ec-quickview-desc">Lorem Ipsum is simply dummy text of the printing and
-                                typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
-                                since the 1500s,</div>
+                            <div class="ec-quickview-desc">{{ isset($productos_color) ? $productos_color[0]->descripcion : '' }}</div>
                             <div class="ec-quickview-price">
-                                <span class="old-price">$100.00</span>
-                                <span class="new-price">$80.00</span>
+                                <span class="new-price">${{ isset($productos_color) ? $productos_color[0]->valor_venta : '' }}</span>
+                                {{-- <span class="old-price">$100.00</span>
+                                <span class="new-price">$80.00</span> --}}
                             </div>
 
                             <div class="ec-pro-variation">
                                 <div class="ec-pro-variation-inner ec-pro-variation-color">
                                     <span>Color</span>
                                     <div class="ec-pro-color">
-                                        <ul class="ec-opt-swatch">
-                                            <li><span style="background-color:#ebbf60;"></span></li>
-                                            <li><span style="background-color:#75e3ff;"></span></li>
-                                            <li><span style="background-color:#11f7d8;"></span></li>
-                                            <li><span style="background-color:#acff7c;"></span></li>
-                                            <li><span style="background-color:#e996fa;"></span></li>
-                                        </ul>
+                                        <select class="form-select"  id="color_producto">
+                                            @isset($productos_color)
+                                            @foreach ($productos_color as $producto)
+                                                <option value="{{$producto->color}}">{{$producto->color}}</option>
+                                            @endforeach
+                                            @endisset
+                                        </select>
                                     </div>
                                 </div>
                                 <div class="ec-pro-variation-inner ec-pro-variation-size ec-pro-size">
-                                    <span>Size</span>
+                                    <span>Talla</span>
                                     <div class="ec-pro-variation-content">
                                         <ul class="ec-opt-size">
-                                            <li class="active"><a href="#" class="ec-opt-sz"
-                                                    data-tooltip="Small">S</a></li>
-                                            <li><a href="#" class="ec-opt-sz" data-tooltip="Medium">M</a>
-                                            </li>
-                                            <li><a href="#" class="ec-opt-sz" data-tooltip="Large">X</a>
-                                            </li>
-                                            <li><a href="#" class="ec-opt-sz" data-tooltip="Extra Large">XL</a>
-                                            </li>
+                                            <li class="active talla"><span>{{isset($productos_color) ? $productos_color[0]->talla : ''}}</span></li>
+                                            @isset($productos_color)
+                                            @foreach ($tallas as $talla )
+                                                @if ($talla->talla != $productos_color[0]->talla)
+                                                    <li class="talla"><span>{{$talla->talla}}</span></li>
+                                                @endif
+                                            @endforeach
+                                            @endisset
                                         </ul>
                                     </div>
+                                   
                                 </div>
                             </div>
                             <div class="ec-quickview-qty">
@@ -84,11 +84,16 @@
                                     <input class="qty-input" type="text" name="ec_qtybtn" value="1" />
                                 </div>
                                 <div class="ec-quickview-cart ">
-                                    <button class="btn btn-primary"><img loading='lazy'
-                                            src="assets/images/icons/cart.svg" class="svg_img pro_svg" alt="" />
-                                        Add To Cart</button>
+                                    @isset($productos_color)
+                                    @if ($productos_color[0]->stock == 0 )
+                                        <button id="addToCart" class="btn btn-primary add-to-cart-product" disabled>Add To Cart</button>
+                                    @else
+                                        <button id="addToCart" class="btn btn-primary add-to-cart-product">Add To Cart</button>
+                                    @endif
+                                    @endisset
                                 </div>
                             </div>
+                            
                         </div>
                     </div>
                 </div>
