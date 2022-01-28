c43a851076f12a9bf711f091b08a03f57e5eeeb1
Christian Ramos <crismas06@outlook.com>
2022-01-28 10:47:11 -0500

    politicas

--- resources/views/ecomerce/politica-privacidad.blade.php
+++ resources/views/ecomerce/politica-privacidad.blade.php
@@ -11,7 +11,7 @@
                         <div class="col-md-6 col-sm-12">
                             <!-- ec-breadcrumb-list start -->
                             <ul class="ec-breadcrumb-list">
-                                <li class="ec-breadcrumb-item"><a href="{{route('web')}}">Home</a></li>
+                                <li class="ec-breadcrumb-item"><a href="{{route('web')}}">Inicio</a></li>
                                 <li class="ec-breadcrumb-item active">Política</li>
                             </ul>
                             <!-- ec-breadcrumb-list end -->
@@ -38,42 +38,85 @@
                     <div class="ec-common-wrapper">
                         <div class="col-sm-12 ec-cms-block">
                             <div class="ec-cms-block-inner">
-                                <h3 class="ec-cms-block-title">Bienvenido a la Pagina web de Ibizza.</h3>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <b>Lorem
-                                        Ipsum is simply dutmmy text</b> ever since the 1500s, when an unknown printer
-                                    took a galley of type and scrambled it to make a type specimen book. It has survived
-                                    not only five centuries, but also the leap into electronic typesetting, remaining
-                                    essentially unchanged. <b>Lorem Ipsum is simply dutmmy text</b></p>
+                                <h3 class="ec-cms-block-title">Políticas de Privacidad.</h3>
+                                <p class="text-justify"><b>Catalogo Ibizza by Dpisar</b> asegura la máxima reserva y protección de aquellos datos de carácter personal (los "Datos Personales") que las empresarias del sitio en Internet de su propiedad ("Empresarias"), ubicado en la dirección electrónica Ibizza.com ("Sitio"), ingresen al sitio con el fin de utilizar los servicios de éste.<br><br>
+                                    Ibizza.com cuenta con la tecnología SSL (Secure Sockets Layer) que asegura la autenticidad del sitio, como el cifrado de toda la información que usted nos entrega. Cada vez que usted se registra en el nuestro sitio, independiente del lugar donde se encuentre, a efectos de comprar un producto, el navegador se conecta a nuestro sitio a través del protocolo SSL que acredita que usted se encuentra efectivamente en nuestro sitio y en nuestros servidores. De esta manera sus datos personales quedan protegidos de ser leídos cuando viajan a través de Internet.
+                                    Ibizza.com proporciona a las Empresarias los recursos técnicos adecuados para que tomen conocimiento de la presente Política de Privacidad y Protección de Datos Personales y de cualquier otra información que pueda ser relevante, constituyendo el ingreso de Datos Personales o información confidencial en el Sitio la manifestación expresa de su consentimiento a la presente Política de Privacidad y Protección de Datos Personales.<br><br>
+                                    <b>Los Datos Personales de los Empresarias</b> no son entregados por Ibizza.com a terceros, en caso de ser solicitado información por terceros debe ser previamente autorizada por las Empresarias de dichos Datos Personales o información confidencial. Sin embargo, su uso con fines estadísticos no requiere de dicha autorización previa.<br><br>
+                                    Ibizza.com puede reunir, analizar y utilizar la información que obtiene de los Empresarias, como un todo colectivo. Así, puede determinar qué áreas son las preferidas o los servicios más utilizados. Dicha información, que sólo se usa en forma anónima y agregada, puede ser revelada a los interesados de la empresa con el fin de poder determinar cómo hacer el Sitio cada vez mejor para sus Empresarias.
+                                    Los Datos Personales que los Empresarias facilitan a Ibiza.com. pueden ser ingresados por medio de un "Registro de Usuario", o a través de la asistencia de un asesor comercial, o agente comercial.<br><br>
+                                    En el caso de que los Empresarias opten por no participar en procesos de Reclutamiento en Línea, sus Datos Personales quedarán almacenados bajo reserva en la base de datos de Ibizza.com 
+                                    Cuando las Empresarias ingresan sus Datos Personales, Ibizza.com abre una página personal para cada Usuario en la cual almacena sus antecedentes, y a la que sólo el mismo Usuario puede ingresar (con su nombre de usuario y clave) en cada oportunidad que requiera revisar, actualizar o modificar sus Datos Personales contenidos en ella (la "Página Personal").<br><br>
+                                    Ibizza.com utilizara la información de contacto ingresada por las Empresarias para el envío de información relativa a los servicios propios del Sitio y eventualmente para la realización de consultas que tengan relación con la calidad del servicio.<br><br>
+                                    Ibizza.com será responsable de la Base de Datos y de los Datos Personales contenidos en cada Página Personal.<br><br>
+                                    Con el objeto de evitar la pérdida, mal uso, alteración, acceso no autorizado y robo de los Datos Personales o Información Confidencial facilitados por los Empresarias, Ibizza.com ha adoptado los niveles de seguridad y de protección de Datos Personales legalmente requeridos, y ha instalado todos los medios y medidas técnicas a su alcance. Sin embargo, las Empresarias deben tener en consideración que los niveles de seguridad en Internet no son perfectos, por lo que Ibizza.com no puede garantizar la total y absoluta inviolabilidad de su Base de Datos, ni el perfecto funcionamiento de sus mecanismos de protección y resguardo.<br><br>
+                                    La cantidad de información que sea ingresada a es voluntaria, sin que el ingresar distintos grados de información implique una diferencia en la calidad o cantidad de los servicios prestados por Ibizza.com, a menos que se indique expresamente lo contrario.<br><br>
+                                    Las Empresarias podrán ejercitar los derechos que esta Política de Privacidad y Protección de Datos Personales señala. Si  Ibizza.com decidiera cambiar su Política de Privacidad y Protección de Datos Personales se lo hará saber debidamente a través del Sitio a todos sus Empresarias y Clientes.</p>
                             </div>
                         </div>
                         <div class="col-sm-12 ec-cms-block">
                             <div class="ec-cms-block-inner">
-                                <h3 class="ec-cms-block-title">Sobre el ecomerce de Ibizza</h3>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <b>Lorem
-                                        Ipsum is simply dutmmy text</b> ever since the 1500s, when an unknown printer
-                                    took a galley of type and scrambled it to make a type specimen book. It has survived
-                                    not only five centuries, but also the leap into electronic typesetting, remaining
-                                    essentially unchanged. <b>Lorem Ipsum is simply dutmmy text</b></p>
+                                <h3 class="ec-cms-block-title">Políticas de entrega y envío</h3>
+                                <p><b>Ibizza ofrece envíos de mercancía a todo el país*.</b> El tiempo de despacho inicia a partir del momento en que sea realizado el cruce financiero del pedido con la respectiva confirmación del pago por: - Pagos Online (compras realizadas con Tarjetas de Crédito o Débito Automático PSE) y/o Transacciones realizadas por depósitos Bancarios.<br><br>
+                                    Los envíos y entregas de pedidos serán realizados a través de empresas prestadoras de servicios logísticos en este caso con Servientrega, u otras a las cuales hubiese lugar y negociación por parte de Ibizza.<br><br>
+                                    Ibizza, publicara e informara el numero de la guía con el cual fue despachado el pedido, con el fin de que el cliente pueda realizar trazabilidad del pedido. Podrá ser consultado en la página web <a href="https://www.servientrega.com.ec/" target="_blank">https://www.servientrega.com.ec/</a> dando clic en <b>Rastreo</b> o comunicarse al 373200 o al correo electrónico <a href="mailto:servicioalcliente@servientrega.com.ec">servicioalcliente@servientrega.com.ec</a> 
+                                    </p>
+                                    <h5 class="ec-cms-block-subtitle text-danger">IMPORTANTE</h5>
+                                    <p>•	Envíos a Nivel Nacional.<br>
+                                        •	El tiempo de entrega será de máximo 24 a 72 horas. *Aplican restricciones de envío a localidades apartadas.<br>
+                                        •	La entrega se efectuará en la dirección de destino informada en el momento del pedido. <br>
+                                        •	En caso que el comprador digite la dirección de entrega errada, el tiempo de entrega puede ser mayor o incluso no llegar el pedido. Ni Ibizza ni el operador logístico se hacen responsables por direcciones de despacho ingresadas erradamente por el cliente.<br>
+                                        •	Si no hubiera una persona que recibiera la mercancía, se intentará nuevamente dentro de las 24 o 48 horas hábiles siguientes.<br>
+                                        •	Posterior al primer intento de entrega, se realiza un intento más, el cual se encarga el área de confirmaciones quien se encarga de notificar al cliente.<br>
+                                        •	Ante la imposibilidad de entrega, el envío regresará a la bodega central y el costo del envío posterior correrá por cuenta del comprador<br>
+                                        •	Antes de recibir su pedido, asegúrese de revisar que el producto no se encuentre abierto o con señales de violación de los sellos de seguridad y/o inventario, de igual manera verifique el estado del embalaje, empaque o cajas. Si encuentra alguna señal de alteración en los mismo, absténgase de recibir el producto y haga la anotación respectiva en la guía de entrega<br>
+                                        •	Una vez firmada la guía del transportista por parte del cliente y/o la persona autorizada, se acepta el recibido del(los) producto(s) a conformidad. 
+                                        </p>
                             </div>
                         </div>
                         <div class="col-sm-12 ec-cms-block">
                             <div class="ec-cms-block-inner">
-                                <h3 class="ec-cms-block-title">¿Cómo funciona la navegación y compra de productos?</h3>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <b>Lorem
-                                        Ipsum is simply dutmmy text</b> ever since the 1500s, when an unknown printer
-                                    took a galley of type and scrambled it to make a type specimen book. It has survived
-                                    not only five centuries, but also the leap into electronic typesetting, remaining
-                                    essentially unchanged. <b>Lorem Ipsum is simply dutmmy text</b></p>
+                                <h3 class="ec-cms-block-title">Políticas de cambios o devoluciones</h3>
+                                <p>Todas las compras realizadas en el Cagtalogo Ibizza by D’pisar cuenta tan con nuestra garantía D’PISAR, la cual permite realizar reemplazos o cambios de mercadería inconforme.<br><br>
+                                    <span class="text-danger">NOTA IMPORTANTE:</span> no se realizan reembolsos o devoluciones de dinero.<br><br>
+                                    
+                                    1.	Los cambios se realizan por Tallas, Color o por Calidad (ver Política de Garantía).<br>
+                                    2.	Los cambios solicitados por tallas y color se deben realizar en un plazo máximo de 10 días de la emisión de la factura y/o envió del pedido.<br>
+                                    3.	Todo producto para cambio debe estar en condiciones óptimas, sin uso, con caja original y con las etiquetas respectivas.<br>
+                                    4.	Para realizar el cambio se debe comunicar con su asesor de ventas para confirmar el stock del producto a cambiar, una vez confirmado el producto se procede a ejecutar el cambio.<br>
+                                    5.	Para hacer efectivo el cambio es indispensable enviar el calzado con la factura.<br>
+                                    6.	El costo del retorno y devolución de envió debe ser asumido por la empresaria quien ha solicitado del cambio. <br>
+                                    7.	El envió del cambio se podrá incluir con el siguiente pedido de la empresaria, en este caso la devolución no tendría ningún costo, solo el retorno del calzado. Si no tiene pedido, debe cancelar el costo del envío un valor de $3.<br>
+                                    8.	El envió se realizará en los 3 días hábiles posteriores que el calzado haya llegado a bodega y procedido con la revisión del estado del producto.<br>
+                                    9.	Los cambios se realizan dentro de la misma campaña.<br>
+                                    10.	Los cambios en locales, se realizan mano a mano, así deberá indicarse al administrador o encargado de local, acompañado de un formato donde firma la empresaria y el administrador de la entrega. Si la empresaria no deja el calzado el vendedor asume el valor del calzado. <br>
+                                    11.	Los cambios se tramitan todos los días<br>
+                                    12.	No se hace reembolso de dinero.<br>
+                                    </p>
                             </div>
                         </div>
                         <div class="col-sm-12 ec-cms-block">
                             <div class="ec-cms-block-inner">
-                                <h3 class="ec-cms-block-title">Convertirse en Empresaria</h3>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <b>Lorem
-                                        Ipsum is simply dutmmy text</b> ever since the 1500s, when an unknown printer
-                                    took a galley of type and scrambled it to make a type specimen book. It has survived
-                                    not only five centuries, but also the leap into electronic typesetting, remaining
-                                    essentially unchanged. <b>Lorem Ipsum is simply dutmmy text</b></p>
+                                <h3 class="ec-cms-block-title">Política de Garantía</h3>
+                                <p>1.	Catalogo Ibizza cuenta con el aval de experiencia de D’pisar, brinda garantía de 30 días (1 mes) de nuestros productos, los cuales cuentan desde la fecha de la factura.<br><br>
+
+                                    2.	Los cambios por garantías se realizarían por Defectos de Fabricación o defecto de Material.
+                                    No aplica garantía por:<br><br>
+                                    •	Descuidos en el uso del calzado<br>
+                                    •	Mantenimiento inadecuado<br>
+                                    •	Deterioro por desgaste natural<br>
+                                    •	Botones, Hebillas y apliques que se desprendieron por mal uso<br><br>
+                                    
+                                    3.	El cambio por garantía se podrá realizar por color, talla o modelo, del mismo valor del producto al que se está realizando el cambio, en caso de seleccionar un calzado de mayor valor, la empresaria deberá realizar el depósito del excedente; si se da el caso contrario, un menor valor, le quedará a la empresaria un saldo a favor. NO se realiza devolución del dinero. <br><br>
+                                    
+                                    4.	Para realizar el cambio se debe enviar el calzado del que se va a realizar el cambio con su respectiva factura.<br><br>
+                                    
+                                    5.	El costo del cambio por garantía es asumido por la empresa, Catalogo Ibizza, para ello la empresaria recibirá una guía de retorno que será enviado por medio del vendedor. Si la empresaria lo envía por sus propios medios no se realizará ningún retorno de dinero.<br><br>
+                                    
+                                    6.	El envió del cambio se podrá incluir con el siguiente pedido de la empresaria, en caso contrario se enviará a los 3 días hábiles de la llegada del pedido a las bodegas de Ibizza.<br><br>
+                                    
+                                    7.	Se podrá realizar el cambio en local, siendo programado y será mano a mano, teniendo en cuenta los 3 días hábiles a partir de la llegada del calzado
+                                    </p>
                             </div>
                         </div>
                     </div>
