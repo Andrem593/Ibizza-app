71319c75db9e967056c190d13a6e9a7082b39342
Christian Ramos <crismas06@outlook.com>
2022-02-02 16:56:44 -0500

    premio ventas

--- resources/views/livewire/modal-premio.blade.php
+++ resources/views/livewire/modal-premio.blade.php
@@ -1,10 +1,12 @@
 <div wire:ignore.self class="modal fade" id="modal_premio" tabindex="-1" role="dialog">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
-            <div class="modal-header text-start bg-ibizza">
-                @isset($premio)
-                    <h5 class="bg-ibizza text-truncate">{{ $premio->descripcion }}</h5>
-                @endisset
+            <div class="modal-header bg-ibizza">
+                <div class="w-100 text-left">
+                    @isset($premio)
+                        <h5 class="bg-ibizza text-truncate">{{ $premio->descripcion }}</h5>
+                    @endisset
+                </div>
     
                 <button type="button" class="btn-close qty_close" data-bs-dismiss="modal" aria-label="Close"></button>
 
@@ -18,7 +20,6 @@
                                 <th>Producto</th>
                                 <th>PVP</th>
                                 <th>Precio Empresaria</th>
-                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -29,7 +30,6 @@
                                                 src="{{ '../storage/images/productos/' . $item->imagen_path }}"
                                                 alt="" width="80" height="80">{{ $item->nombre_mostrar }}</td>
                                         <td class="text-center">$ {{ $item->valor_venta }}</td>
-                                        <td class="text-center">$ {{ $item->precio_empresaria }}</td>
                                         <td class="text-center"><span class="badge bg-ibizza">Gratis</span></td>
                                     </tr>
                                 @endforeach
