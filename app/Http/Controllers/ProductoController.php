01cae0bc26fa9f290490ea86cb87b19991e819f6
Christian Ramos <crismas06@outlook.com>
2022-02-08 14:57:13 -0500

    update productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -152,45 +152,53 @@ class ProductoController extends Controller
         foreach ($data as $key => $row) {
             if ($key >= 1) {
 
-                $marca_id = DB::table('marcas')->where('nombre', 'like', '%' . $row[3] . '%')->value('id');
+                $producto = Producto::where('sku', $row[0])->first();
 
-                if (empty($marca_id)) {
-                    $marca_id = DB::table('marcas')->insertGetId(
-                        array('nombre' => $row[3], 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s'))
+                if ($producto) {
+                    $producto->stock = $row[11] + $producto->stock;
+                    $producto->save();
+                }else{
+                    $marca_id = DB::table('marcas')->where('nombre', 'like', '%' . $row[3] . '%')->value('id');
+    
+                    if (empty($marca_id)) {
+                        $marca_id = DB::table('marcas')->insertGetId(
+                            array('nombre' => $row[3], 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s'))
+                        );
+                    }
+    
+                    $proveedor_id = DB::table('proveedores')->where('nombre', 'like', '%' . $row[7] . '%')->value('id');
+    
+                    if (empty($proveedor_id)) {
+                        $proveedor_id = DB::table('proveedores')->insertGetId(
+                            array('nombre' => $row[7], 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s'))
+                        );
+                    }
+    
+                    $insert_data = array(
+                        'sku'  => $row[0],
+                        'nombre_producto'  => $row[1],
+                        'descripcion'  => $row[2],
+                        'marca_id'  => $marca_id,
+                        'grupo' => $row[4],
+                        'seccion'  => $row[5],
+                        'clasificacion'  => $row[6],
+                        'proveedor_id'  => $proveedor_id,
+                        'estilo'  => $row[8],
+                        'color'  => $row[9],
+                        'talla'  => $row[10],
+                        'cantidad_inicial'  => $row[11],
+                        'stock'  => $row[11],
+                        'valor_venta'  => $row[12],
+                        'nombre_mostrar'  => $row[13],
+                        'categoria'  => $row[14],
+                        'subcategoria'  => $row[15],
+                        'precio_empresaria'  => $row[16],
+                        'descuento'  => $row[17]
                     );
+    
+                    Producto::create($insert_data);
                 }
 
-                $proveedor_id = DB::table('proveedores')->where('nombre', 'like', '%' . $row[7] . '%')->value('id');
-
-                if (empty($proveedor_id)) {
-                    $proveedor_id = DB::table('proveedores')->insertGetId(
-                        array('nombre' => $row[7], 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s'))
-                    );
-                }
-
-                $insert_data = array(
-                    'sku'  => $row[0],
-                    'nombre_producto'  => $row[1],
-                    'descripcion'  => $row[2],
-                    'marca_id'  => $marca_id,
-                    'grupo' => $row[4],
-                    'seccion'  => $row[5],
-                    'clasificacion'  => $row[6],
-                    'proveedor_id'  => $proveedor_id,
-                    'estilo'  => $row[8],
-                    'color'  => $row[9],
-                    'talla'  => $row[10],
-                    'cantidad_inicial'  => $row[11],
-                    'stock'  => $row[11],
-                    'valor_venta'  => $row[12],
-                    'nombre_mostrar'  => $row[13],
-                    'categoria'  => $row[14],
-                    'subcategoria'  => $row[15],
-                    'precio_empresaria'  => $row[16],
-                    'descuento'  => $row[17]
-                );
-
-                Producto::create($insert_data);
             }
         }
 
