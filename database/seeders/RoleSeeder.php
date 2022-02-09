e78f6aa9004b2b611367a015b5ce4ba1fb859e3b
Christian Ramos <crismas06@outlook.com>
2022-02-09 14:21:28 -0500

    menu

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -27,91 +27,110 @@ class RoleSeeder extends Seeder
 
         Permission::create([
             'name' => 'producto.upload',
-            'description' => 'Carga de Productos'
+            'description' => 'Carga de Productos',
+            'categoria' => 'productos'
         ])->syncRoles([$role1]);
         Permission::create([
             'name' => 'producto.index',
-            'description' => 'Lista de Productos'
+            'description' => 'Lista de Productos',
+            'categoria' => 'productos'
         ])->syncRoles([$role1]);
         Permission::create([
             'name' => 'producto.estilos',
-            'description' => 'Estilos de Productos'
+            'description' => 'Estilos de Productos',
+            'categoria' => 'productos'
         ])->syncRoles([$role1]);
         Permission::create([
             'name' => 'marcas.index',
-            'description' => 'Lista de Marcas'
+            'description' => 'Lista de Marcas',
+            'categoria' => 'productos'
         ])->syncRoles([$role1]);
         Permission::create([
             'name' => 'proveedores.index',
-            'description' => 'Lista de proveedores'
+            'description' => 'Lista de proveedores',
+            'categoria' => 'productos'
         ])->syncRoles([$role1]);
         Permission::create([
             'name' => 'producto.stock-faltante',
-            'description' => 'Historial de stock faltante'
+            'description' => 'Historial de stock faltante',
+            'categoria' => 'productos'
         ])->syncRoles([$role1]);
 
         // Modulo Ventas
 
         Permission::create([
             'name' => 'venta.upload',
-            'description' => 'Carga de Ventas Facturadas'
+            'description' => 'Carga de Ventas Facturadas',
+            'categoria' => 'ventas'
         ])->syncRoles([$role1]);
         Permission::create([
             'name' => 'venta.index',
-            'description' => 'Registros de ventas'
+            'description' => 'Registros de ventas',
+            'categoria' => 'ventas'
         ])->syncRoles([$role1]);
         Permission::create([
             'name' => 'venta.pedido',
-            'description' => 'Toma de pedidos'
+            'description' => 'Toma de pedidos',
+            'categoria' => 'ventas'
         ])->syncRoles([$role1]);
         Permission::create([
             'name' => 'venta.pedidos-guardados',
-            'description' => 'Pedidos guardados'
+            'description' => 'Pedidos guardados',
+            'categoria' => 'ventas'
         ])->syncRoles([$role1]);
 
         // modulo de catalogos
 
         Permission::create([
             'name' => 'catalogos.index',
-            'description' => 'Crear catálogos'
+            'description' => 'Crear catálogos',
+            'categoria' => 'catalogos'
         ])->syncRoles([$role1]);
         Permission::create([
             'name' => 'catalogo.catalogoProducto',
-            'description' => 'Asignar Productos'
+            'description' => 'Asignar Productos',
+            'categoria' => 'catalogos'
         ])->syncRoles([$role1]);
         Permission::create([
             'name' => 'premios.index',
-            'description' => 'Administración de Premios'
+            'description' => 'Administración de Premios',
+            'categoria' => 'catalogos'
         ])->syncRoles([$role1]);        
         
         // MODULO DE USUARIOS
 
         Permission::create([
             'name' => 'roles.index',
-            'description' => 'Administración de Roles'
+            'description' => 'Administración de Roles',
+            'categoria' => 'usuarios'
         ])->syncRoles([$role1]);   
         Permission::create([
             'name' => 'usuario.index',
-            'description' => 'Administración de Usuarios'
+            'description' => 'Administración de Usuarios',
+            'categoria' => 'usuarios'
         ])->syncRoles([$role1]);  
         Permission::create([
             'name' => 'empresarias.index',
-            'description' => 'Administración de Empresarias'
+            'description' => 'Administración de Empresarias',
+            'categoria' => 'usuarios'
         ])->syncRoles([$role1]);  
 
         // MODULO DE REPORTES
 
         Permission::create([
             'name' => 'reportes.index',
-            'description' => 'Reportes de Vendedor'
+            'description' => 'Reportes de Vendedor',
+            'categoria' => 'reportes'
         ])->syncRoles([$role1]);  
         Permission::create([
             'name' => 'reporte.graficos',
-            'description' => 'Reportes Administrativos'
+            'description' => 'Reportes Administrativos',
+            'categoria' => 'reportes'
         ])->syncRoles([$role1]);
         Permission::create([
             'name' => 'reporte.ventas',
-            'description' => 'Reportes de Ventas'
+            'description' => 'Reportes de Ventas',
+            'categoria' => 'reportes'
         ])->syncRoles([$role1]);
     }
 }
